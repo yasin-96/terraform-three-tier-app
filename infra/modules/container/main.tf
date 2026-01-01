7514c651b89ccc7f84b92fd69b27686f211d6c81
Yasin Eraslan <eraslan@dars-hub.com>
2026-01-01 15:21:00 +0100

    nginx-test

--- infra/modules/container/main.tf
+++ infra/modules/container/main.tf
@@ -229,8 +229,8 @@ resource "aws_security_group" "lb" {
   }
 
   egress { # to tasks on container port
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
